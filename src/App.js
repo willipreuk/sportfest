c7eaf6f65aa73be66bd6d8e19696f904c7f81f82
Willi Preuk <willi.preuk@hnb-holding.com>
2019-12-17 23:16:13 +0100

    apollo-client

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import { CssBaseline } from '@material-ui/core';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000',
+});
 
 function App() {
   return (
     <>
       <CssBaseline />
-      <h1>Sportfest</h1>
+      <ApolloProvider client={client}>
+        <h1>Sportfest</h1>
+      </ApolloProvider>
     </>
   );
 }
