28450a1d69ff327e3feaa39f6878d35e3e7d5dda
Willi Preuk <willi.preuk@hnb-holding.com>
2019-12-21 15:07:20 +0100

    Links in Menu

--- src/components/Navigation/listItems.js
+++ src/components/Navigation/listItems.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
@@ -11,56 +12,41 @@ import SportsIcon from '@material-ui/icons/Sports';
 import FormatListNumberedIcon from '@material-ui/icons/FormatListNumbered';
 import ContactsIcon from '@material-ui/icons/Contacts';
 
-
+const mainItems = [
+  { to: '/', icon: <DashboardIcon />, text: 'Home' },
+  { to: '/ergebnisse', icon: <PlaylistAddCheckIcon />, text: 'Ergebnisse' },
+  { to: '/schueler', icon: <PersonIcon />, text: 'Schüler' },
+  { to: '/klassen', icon: <GroupIcon />, text: 'Klassen' },
+];
 export const mainListItems = (
   <div>
-    <ListItem button>
-      <ListItemIcon>
-        <DashboardIcon />
-      </ListItemIcon>
-      <ListItemText primary="Home" />
-    </ListItem>
-    <ListItem button>
-      <ListItemIcon>
-        <PlaylistAddCheckIcon />
-      </ListItemIcon>
-      <ListItemText primary="Ergebnisse" />
-    </ListItem>
-    <ListItem button>
-      <ListItemIcon>
-        <PersonIcon />
-      </ListItemIcon>
-      <ListItemText primary="Schüler" />
-    </ListItem>
-    <ListItem button>
-      <ListItemIcon>
-        <GroupIcon />
-      </ListItemIcon>
-      <ListItemText primary="Klassen" />
-    </ListItem>
+    {mainItems.map((i) => (
+      <ListItem button component={Link} to={i.to}>
+        <ListItemIcon>
+          {i.icon}
+        </ListItemIcon>
+        <ListItemText primary={i.text} />
+      </ListItem>
+    ))}
   </div>
 );
+
+const secondItems = [
+  { to: '/disziplinen', icon: <SportsIcon />, text: 'Disziplinen' },
+  { to: '/massstaebe', icon: <FormatListNumberedIcon />, text: 'Maßstäbe' },
+  { to: '/user', icon: <ContactsIcon />, text: 'User' },
+];
 
 export const secondaryListItems = (
   <div>
     <ListSubheader inset>Einstellungen</ListSubheader>
-    <ListItem button>
-      <ListItemIcon>
-        <SportsIcon />
-      </ListItemIcon>
-      <ListItemText primary="Disziplinen" />
-    </ListItem>
-    <ListItem button>
-      <ListItemIcon>
-        <FormatListNumberedIcon />
-      </ListItemIcon>
-      <ListItemText primary="Maßstäbe" />
-    </ListItem>
-    <ListItem button>
-      <ListItemIcon>
-        <ContactsIcon />
-      </ListItemIcon>
-      <ListItemText primary="User" />
-    </ListItem>
+    {secondItems.map((i) => (
+      <ListItem button component={Link} to={i.to}>
+        <ListItemIcon>
+          {i.icon}
+        </ListItemIcon>
+        <ListItemText primary={i.text} />
+      </ListItem>
+    ))}
   </div>
 );
--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Route, Redirect } from 'react-router-dom';
+import Layout from './Layout';
 
 const PrivateRoute = ({ path, Component, exact }) => {
   const jwt = localStorage.getItem('jwt');
-  return <Route exact={exact} path={path} render={() => (jwt ? <Component /> : <Redirect to="/login" />)} />;
+  return <Route exact={exact} path={path} render={() => (jwt ? <Layout><Component /></Layout> : <Redirect to="/login" />)} />;
 };
 PrivateRoute.propTypes = {
   path: PropTypes.string.isRequired,
