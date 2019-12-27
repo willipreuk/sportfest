1e468da055b927ad23974acb0c0d6fdc2ebc3c71
Willi Preuk <willi.preuk@hnb-holding.com>
2019-12-27 17:19:41 +0100

    redux dev tools

--- src/reducers/ergebnis.js
+++ src/reducers/ergebnis.js
@@ -12,8 +12,8 @@ export default (s = initialState, action) => {
   const state = cloneDeep(s);
   switch (action.type) {
     case ERGEBNIS_SET_RANGE: {
-      if (action.payload.von) state.filter.von = action.von;
-      if (action.payload.bis) state.filter.bis = action.bis;
+      if (action.payload.von) state.filter.von = action.payload.von;
+      if (action.payload.bis) state.filter.bis = action.payload.bis;
       break;
     }
     default: return state;
