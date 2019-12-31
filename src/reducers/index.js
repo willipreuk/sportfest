385ba9b48078a7fb7b49163147d6ed39094143ae
Willi Preuk <willi.preuk@hnb-holding.com>
2019-12-31 15:25:00 +0100

    connected router and redux

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,14 @@
 import { combineReducers } from 'redux';
+import { connectRouter } from 'connected-react-router';
 import ergebnis from './ergebnis';
 import user from './user';
 import navigation from './navigation';
 
-export default combineReducers({ ergebnis, user, navigation });
+const createRootReducer = (history) => combineReducers({
+  ergebnis,
+  user,
+  navigation,
+  router: connectRouter(history),
+});
+
+export default createRootReducer;
