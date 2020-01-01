dfa509682d1715ed7852f191f947b6707b19c4e9
Willi Preuk <willi.preuk@hnb-holding.com>
2020-01-01 22:52:07 +0100

    schreiber init page

--- src/routes/Schreiber/index.js
+++ src/routes/Schreiber/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Container, makeStyles, Typography } from '@material-ui/core';
+import { useSelector } from 'react-redux';
+import Init from './Init';
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -8,17 +10,25 @@ const useStyles = makeStyles((theme) => ({
     flexDirection: 'column',
     alignItems: 'center',
   },
+  heading: {
+    textAlign: 'center',
+  },
 }));
 
 export default () => {
   const classes = useStyles();
+  const { disziplin, klasse } = useSelector((state) => state.schreiber);
 
   return (
     <Container maxWidth="xs" component="main">
       <div className={classes.paper}>
-        <Typography component="h1" variant="h5">
+        <Typography component="h1" variant="h4">
           {`Sportfest ${new Date().getFullYear()}`}
         </Typography>
+        <Typography variant="h5" className={classes.heading}>
+          Giebichenstein-Gymnasium &quot;Thomas Müntzer&quot;
+        </Typography>
+        {!disziplin && !klasse ? <Init /> : null}
       </div>
     </Container>
   );
