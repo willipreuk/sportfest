973a1aaaf074153db88bd82a3e9e72e06d74003f
Willi Preuk <willi.preuk@hnb-holding.com>
2020-03-20 23:47:52 +0100

    setLoading reducer

---
+++ src/reducers/app-state.js
@@ -0,0 +1,18 @@
+import { cloneDeep } from 'lodash';
+import { APP_STATE_SET_LOADING } from '../actions/types';
+
+const initialState = {
+  loading: false,
+};
+
+export default (s = initialState, action) => {
+  const state = cloneDeep(s);
+  switch (action.type) {
+    case APP_STATE_SET_LOADING: {
+      state.loading = action.payload;
+      break;
+    }
+    default: return s;
+  }
+  return state;
+};
---
+++ src/reducers/app-state.test.js
@@ -0,0 +1,35 @@
+import appState from './app-state';
+import { APP_STATE_SET_LOADING } from '../actions/types';
+import { setLoading } from '../actions/app-state';
+
+describe('app-state reducer', () => {
+  it('should return initial state', () => {
+    expect.assertions(1);
+    expect(appState(undefined, {})).toStrictEqual({
+      loading: false,
+    });
+  });
+  describe('action setLoading', () => {
+    it('should, return action object', () => {
+      expect.assertions(1);
+      expect(setLoading(false)).toStrictEqual({
+        type: APP_STATE_SET_LOADING,
+        payload: false,
+      });
+    });
+    it('should set to true', () => {
+      expect.assertions(1);
+      expect(appState(
+        undefined,
+        { type: APP_STATE_SET_LOADING, payload: true },
+      )).toStrictEqual({ loading: true });
+    });
+    it('should set to false', () => {
+      expect.assertions(1);
+      expect(appState(
+        undefined,
+        { type: APP_STATE_SET_LOADING, payload: false },
+      )).toStrictEqual({ loading: false });
+    });
+  });
+});
