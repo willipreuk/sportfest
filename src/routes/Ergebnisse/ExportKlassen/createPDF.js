d4d5d308bee9f0478f1c61ab6106b52cdf6416eb
willi.preuk <willi@preuk.eu>
2020-04-10 23:15:06 +0200

    generate pdf

--- src/routes/Ergebnisse/ExportKlassen/createPDF.js
+++ src/routes/Ergebnisse/ExportKlassen/createPDF.js
@@ -1,3 +1,48 @@
-export default (von, bis) => {
+import * as JsPDF from 'jspdf';
+import 'jspdf-autotable';
 
+JsPDF.API.setFontHeading = function setFontHeading() {
+  this.setFontStyle('bold');
+  this.setFontSize(24);
+};
+
+const renderKlasse = (klasse, doc) => {
+  doc.addPage();
+  doc.setFontHeading();
+  doc.text('Ergebnisse', 210 / 2, 30, { align: 'center' });
+  doc.setFontStyle('normal');
+  doc.text(`Klasse ${klasse.klasse.stufe}/${klasse.klasse.name}`, 210 / 2, 40, { align: 'center' });
+  doc.autoTable({
+    head: [['Vorname', 'Nachname', 'Punkte', 'Note']],
+    body: klasse.schuelerAuswertung.map((schueler) => ([
+      schueler.schueler.vorname,
+      schueler.schueler.nachname,
+      schueler.punkte,
+      schueler.note,
+    ])),
+    startY: 55,
+  });
+};
+
+export default (data, von, bis) => {
+  const doc = new JsPDF({
+    // A4 = 210mm * 297mm
+    format: 'a4',
+  });
+  doc.setFontHeading();
+  doc.text('Auswertung Sportfest', 210 / 2, 30, { align: 'center' });
+  doc.text(`${new Date().getFullYear()}`, 210 / 2, 40, { align: 'center' });
+
+  doc.setFontStyle('normal');
+  doc.setFontSize(18);
+  doc.text(`Klassenstufen: ${von} - ${bis}`, 210 / 2, 70, { align: 'center' });
+
+  data.besteKlassen.forEach((klasse) => {
+    // prüfen ob ergebnisse vorliegen
+    if (klasse.durchschnitt) {
+      renderKlasse(klasse, doc);
+    }
+  });
+
+  doc.output('dataurlnewwindow');
 };
--- src/routes/Ergebnisse/ExportKlassen/index.js
+++ src/routes/Ergebnisse/ExportKlassen/index.js
@@ -3,6 +3,8 @@ import { useSelector } from 'react-redux';
 import { Typography, Button } from '@material-ui/core';
 import { CloudDownload as CloudDownloadIcon } from '@material-ui/icons';
 import { makeStyles } from '@material-ui/core/styles';
+import gql from 'graphql-tag';
+import { useQuery } from '@apollo/react-hooks';
 import createPDF from './createPDF';
 
 const useStyles = makeStyles((theme) => ({
@@ -11,9 +13,34 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+const AUSWERTUNG_STUFEN = gql`
+    query AuswertungStufen($von: Int!, $bis: Int!) {
+        auswertungStufen(von: $von, bis: $bis) {
+            besteKlassen {
+                klasse {
+                    name
+                    stufe
+                }
+                durchschnitt
+                schuelerAuswertung {
+                    schueler {
+                        nachname
+                        vorname
+                    }
+                    note
+                    punkte
+                }
+            }
+        } 
+    }
+`;
+
 export default () => {
   const classes = useStyles();
-  const { von, bis } = useSelector((state) => state.filter);
+  const { von, bis } = useSelector((state) => state.ergebnis.filter);
+  const { loading, data } = useQuery(AUSWERTUNG_STUFEN, { variables: { von, bis } });
+
+  if (loading && !data) return null;
   return (
     <>
       <Typography variant="h6">Alle Klassen exportieren</Typography>
@@ -25,7 +52,7 @@ export default () => {
         variant="contained"
         color="primary"
         startIcon={<CloudDownloadIcon />}
-        onClick={createPDF(von, bis)}
+        onClick={() => createPDF(data.auswertungStufen, von, bis)}
       >
         Download
       </Button>
