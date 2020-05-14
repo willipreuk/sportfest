95040db8a1fc05067ce1d924a71037a05aa66139
willi.preuk <willi@preuk.eu>
2020-05-14 21:39:17 +0200

    download template

--- src/routes/Schueler/SchuelerUpload/index.js
+++ src/routes/Schueler/SchuelerUpload/index.js
@@ -1,9 +1,10 @@
 import React, { useRef, useState } from 'react';
-import { Button, makeStyles, Typography } from '@material-ui/core';
+import { Button, makeStyles, Typography, Grid, Paper } from '@material-ui/core';
 import gql from 'graphql-tag';
 import { useMutation } from '@apollo/react-hooks';
 import LoadingSpinner from '../../../components/LoadingSpinner';
 import AlertDialog from './AlertDialog';
+import template from '../../../assets/schueler/template.csv';
 
 const useStyles = makeStyles((theme) => ({
   title: {
@@ -14,6 +15,12 @@ const useStyles = makeStyles((theme) => ({
   },
   uploadButton: {
     marginTop: theme.spacing(2),
+  },
+  paper: {
+    paddingLeft: theme.spacing(3),
+    paddingTop: theme.spacing(2),
+    paddingBottom: theme.spacing(2),
+    minHeight: theme.spacing(15),
   },
 }));
 
@@ -51,27 +58,48 @@ export default () => {
   if (loading) return <LoadingSpinner />;
 
   return (
-    <>
-      <Typography variant="h6" className={classes.title}>
-        Schülerupload
-      </Typography>
-      <Typography color="error">Achtung: Dabei gehen alle Schülerdaten verloren!</Typography>
-      <Button
-        className={classes.uploadButton}
-        component="label"
-        variant="contained"
-        color="primary"
-      >
-        Schülerdaten hochladen
-        <input
-          type="file"
-          onChange={onChange}
-          accept="text/csv"
-          className={classes.input}
-          ref={fileInput}
-        />
-      </Button>
-      <AlertDialog setOpen={setOpen} submit={submit} open={open} />
-    </>
+    <Grid container spacing={3}>
+      <Grid item xs={12} md={6}>
+        <Paper className={classes.paper}>
+          <Typography variant="h6" className={classes.title}>
+            Schülerupload
+          </Typography>
+          <Typography color="error">Achtung: Dabei gehen alle Schülerdaten verloren!</Typography>
+          <Button
+            className={classes.uploadButton}
+            component="label"
+            variant="contained"
+            color="primary"
+          >
+            Schülerdaten hochladen
+            <input
+              type="file"
+              onChange={onChange}
+              accept="text/csv"
+              className={classes.input}
+              ref={fileInput}
+            />
+          </Button>
+          <AlertDialog setOpen={setOpen} submit={submit} open={open} />
+        </Paper>
+      </Grid>
+      <Grid item xs={12} md={6}>
+        <Paper className={classes.paper}>
+          <Typography variant="h6" className={classes.title}>
+            Vorlage zum Upload
+          </Typography>
+          <Typography>Laden Sie hier die Vorlage für die Schülerdaten herunter.</Typography>
+          <Button
+            className={classes.uploadButton}
+            variant="contained"
+            color="primary"
+            component="a"
+            href={template}
+          >
+            Herunterladen
+          </Button>
+        </Paper>
+      </Grid>
+    </Grid>
   );
 };
